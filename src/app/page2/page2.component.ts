28e59b439c60c02ca7bdc71c006c43e3c344e7ef
Will <doggy.huang@gmail.com>
2019-11-05 14:50:29 +0800

    示範 queryParamMap 的用法

--- src/app/page2/page2.component.ts
+++ src/app/page2/page2.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, NavigationExtras } from '@angular/router';
 
 @Component({
   selector: 'app-page2',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Page2Component implements OnInit {
 
-  constructor() { }
+  id = 0;
+
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    // this.id = +this.route.snapshot.queryParamMap.get('id');
+    this.route.queryParamMap.subscribe((param) => {
+      this.id = +param.get('id');
+    });
+  }
+
+  inc(num: number) {
+    // this.router.navigateByUrl('/page2?id=' + (this.id + num));
+
+    const options: NavigationExtras = { queryParams: { id: this.id + num } };
+    this.router.navigate(['/page2'], options);
   }
 
 }
