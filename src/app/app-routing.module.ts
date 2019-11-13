b461b7f531ecd149bbccd2010a0a4d70e916a7cf
Will <doggy.huang@gmail.com>
2019-11-13 11:20:06 +0800

    加入 enableTracing 與 scrollPositionRestoration 設定到 forRoot 路由模組中

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -33,7 +33,9 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    useHash: true
+    useHash: true,
+    enableTracing: true,
+    scrollPositionRestoration: 'enabled'
   })],
   exports: [RouterModule]
 })
