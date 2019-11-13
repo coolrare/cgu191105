9b01cd9375fd4168e48c9ea445a5943e1ab1bc35
Will <doggy.huang@gmail.com>
2019-11-13 12:22:02 +0800

    示範 Template-driven Form 基本設定

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  data: any = {
+    username: 'Will',
+    password: '123123'
+  };
+
   constructor() { }
 
   ngOnInit() {
