bdadceb0921798f849ae8cce97fc09f4a2074351
Will <doggy.huang@gmail.com>
2019-11-20 10:52:28 +0800

    示範如何撰寫簡易的驗證器

--- src/app/login2/login2.component.ts
+++ src/app/login2/login2.component.ts
@@ -1,5 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, NgForm, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, NgForm, Validators, FormControl } from '@angular/forms';
+
+function checkTWID(control: FormControl) {
+  if (checkID(control.value)) {
+    return null;
+  } else {
+    return {
+      TWID: false
+    };
+  }
+}
+
 
 @Component({
   selector: 'app-login2',
@@ -15,7 +26,7 @@ export class Login2Component implements OnInit {
     this.form = this.fb.group({
       email: [
         '',
-        [Validators.required, Validators.email]
+        [Validators.required, checkTWID]
       ],
       password: [
         '',
@@ -25,6 +36,33 @@ export class Login2Component implements OnInit {
   }
 
   onSubmit(ngform: NgForm) {
+    if (this.form.valid) {
+      // TODO
+    }
   }
 
+}
+
+
+
+
+
+function checkID(id: string) {
+  const tab = 'ABCDEFGHJKLMNPQRSTUVXYWZIO'
+  const A1 = new Array(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3);
+  const A2 = new Array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5);
+  const Mx = new Array(9, 8, 7, 6, 5, 4, 3, 2, 1, 1);
+
+  if (id.length !== 10) { return false; }
+  let i = tab.indexOf(id.charAt(0));
+  if (i === -1) { return false; }
+  let sum = A1[i] + A2[i] * 9;
+
+  for (i = 1; i < 10; i++) {
+    const v = parseInt(id.charAt(i), 10);
+    if (isNaN(v)) { return false; }
+    sum = sum + v * Mx[i];
+  }
+  if (sum % 10 !== 0) { return false; }
+  return true;
 }
